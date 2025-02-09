819e331fd7bb832736fae920a947d41e161b338f
Umar Sharief <noneofyourbusiness1415252@users.noreply.github.com>
2025-02-09 12:40:52 +0000

    fix(numeracy): make all answers positive integers todo: simple decimals at later levels

--- src/games/numeracy/expression.rs
+++ src/games/numeracy/expression.rs
@@ -8,21 +8,21 @@ pub struct Expression {
 
 impl Expression {
     pub fn new(level: u32) -> Self {
-        let ops = ["+", "-", "*", "/"];
+        let ops = ['+', '-', '*', '/'];
         
         let complexity = (level as f64 * 1.5).ceil() as i32;
-        let a = (Math::random() * (complexity * 5) as f64).floor() as i32 + 1;
+        let a = (Math::random() * (complexity * 8) as f64).floor() as i32 + complexity;
         let b = (Math::random() * (complexity * 3) as f64).floor() as i32 + 1;
         let op = ops[(Math::random() * 4.0).floor() as usize];
         
         let (text, value) = match op {
-            "+" => (format!("{} + {}", a, b), (a + b) as f64),
-            "-" => (format!("{} - {}", a, b), (a - b) as f64),
-            "*" => (format!("{} × {}", a, b), (a * b) as f64),
-            "/" => {
+            '+' => (format!("{} + {}", a, b), (a + b) as f64),
+            '-' => (format!("{} - {}", a, b), (a - b) as f64), // a is always > b
+            '*' => (format!("{} × {}", a, b), (a * b) as f64),
+            '/' => {
                 let product = a * b;
-                (format!("{} ÷ {}", product, a), b as f64)
-            }
+                (format!("{} ÷ {}", product, b), a as f64) // Division always results in a
+            },
             _ => unreachable!(),
         };
 
