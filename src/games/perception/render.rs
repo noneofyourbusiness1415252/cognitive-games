6f50d4f6bd23c3a90c4e2769068e9c95660185d4
Umar Sharief <80610051+noneofyourbusiness1415252@users.noreply.github.com>
2025-02-14 18:19:49 +0000

    ?

--- src/games/perception/render.rs
+++ src/games/perception/render.rs
@@ -55,7 +55,7 @@ impl Perception {
         Ok(())
     }
 
-    pub fn update_cell_state(&self, cell: &Element, x: usize, y: usize) -> Result<(), JsValue> {
+    pub(super) fn update_cell_state(&self, cell: &Element, x: usize, y: usize) -> Result<(), JsValue> {
         // Reset base class
         cell.set_class_name("cell");
 
